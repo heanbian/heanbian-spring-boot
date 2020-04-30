8a6212a8f8c85b3adb0259247d71de93471c6219
Administrator <Administrator@172.16.0.4>
2020-04-30 22:56:24 +0800

    1

--- heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/email/EmailProperties.java
+++ heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/email/EmailProperties.java
@@ -5,34 +5,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "heanbian.email")
 public class EmailProperties {
 
-	/**
-	 * 代理邮件主机地址
-	 */
 	private String host;
 
-	/**
-	 * 端口号
-	 */
 	private int port;
 
-	/**
-	 * 用户名
-	 */
 	private String username;
 
-	/**
-	 * 密码
-	 */
 	private String password;
 
-	/**
-	 * 发送方，默认{@link #username}
-	 */
 	private String from;
 
-	/**
-	 * 是否Debug
-	 */
 	private boolean debug;
 
 	private boolean enabled = false;
