b4e4843db7a4aafc489bdc1dcac315237112ee90
Administrator <Administrator@172.16.0.4>
2020-04-30 17:12:52 +0800

    1

---
+++ heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/email/EmailProperties.java
@@ -0,0 +1,95 @@
+package com.heanbian.boot.autoconfigure.email;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "heanbian.email")
+public class EmailProperties {
+	/**
+	 * 代理邮件主机地址
+	 */
+	private String host;
+
+	/**
+	 * 端口号
+	 */
+	private int port;
+
+	/**
+	 * 用户名
+	 */
+	private String username;
+
+	/**
+	 * 密码
+	 */
+	private String password;
+
+	/**
+	 * 发送方，默认{@link #username}
+	 */
+	private String from;
+
+	/**
+	 * 是否Debug
+	 */
+	private boolean debug;
+
+	private boolean enable = false;
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public boolean isDebug() {
+		return debug;
+	}
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+}
