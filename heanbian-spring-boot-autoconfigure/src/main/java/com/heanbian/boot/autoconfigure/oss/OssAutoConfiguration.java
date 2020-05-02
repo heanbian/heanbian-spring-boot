dbf0666064fc7c8f4a95f3bec1c59b3a347c7f8d
heanbian <heanbian@heanbian.com>
2020-05-02 12:10:11 +0800

    1

--- heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/oss/OssAutoConfiguration.java
+++ heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/oss/OssAutoConfiguration.java
@@ -1,15 +1,20 @@
 package com.heanbian.boot.autoconfigure.oss;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 
 @Configuration
 @EnableConfigurationProperties(OssProperties.class)
 public class OssAutoConfiguration {
 
-//	@Bean
-//	@ConditionalOnProperty(prefix = "heanbian.oss", name = "enable", havingValue = "true")
-//	public OssTemplate ossTemplate(OssProperties op) {
-//		return new OssTemplate(op);
-//	}
+	@Bean
+	@ConditionalOnProperty(prefix = "heanbian.oss", name = "enable", havingValue = "true")
+	public OSS oss(OssProperties op) {
+		return new OSSClientBuilder().build(op.getEndpoint(), op.getAccessKeyId(), op.getAccessKeySecret());
+	}
 }
