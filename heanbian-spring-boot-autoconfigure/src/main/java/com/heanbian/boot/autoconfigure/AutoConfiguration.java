395f5fe3823ff5104011d6fd813be5bdf3416e6f
Heanbian <mahingtian@gmail.com>
2023-01-18 21:46:36 +0800

    1
    1

--- heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/AutoConfiguration.java
+++ heanbian-spring-boot-autoconfigure/src/main/java/com/heanbian/boot/autoconfigure/AutoConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClientBuilder;
 import com.heanbian.block.crypto.AesTemplate;
 import com.heanbian.block.crypto.EcTemplate;
 import com.heanbian.block.crypto.ExRsaTemplate;
@@ -21,56 +19,50 @@ import com.heanbian.block.jwt.JwtTemplate;
 @EnableConfigurationProperties({ //
 		com.heanbian.boot.autoconfigure.ElasticsearchProperties.class, //
 		com.heanbian.boot.autoconfigure.EmailProperties.class, //
-		com.heanbian.boot.autoconfigure.OAuth2Properties.class, //
-		com.heanbian.boot.autoconfigure.OssProperties.class })
+		com.heanbian.boot.autoconfigure.OAuth2Properties.class })
 public class AutoConfiguration {
 
 	@Bean
 	@ConditionalOnProperty(prefix = "heanbian.elasticsearch", name = "enabled", havingValue = "true")
-	public ElasticsearchTemplate elasticsearchTemplate(com.heanbian.boot.autoconfigure.ElasticsearchProperties ep) {
+	ElasticsearchTemplate elasticsearchTemplate(com.heanbian.boot.autoconfigure.ElasticsearchProperties ep) {
 		return new ElasticsearchTemplate(ep.clusterNodes());
 	}
 
 	@Bean
 	@ConditionalOnClass(AesTemplate.class)
-	public AesTemplate aesTemplate() {
+	AesTemplate aesTemplate() {
 		return new AesTemplate();
 	}
 
 	@Bean
 	@ConditionalOnClass(RsaTemplate.class)
-	public RsaTemplate rsaTemplate() {
+	RsaTemplate rsaTemplate() {
 		return new RsaTemplate();
 	}
-	
+
 	@Bean
 	@ConditionalOnClass(ExRsaTemplate.class)
-	public ExRsaTemplate exRsaTemplate() {
+	ExRsaTemplate exRsaTemplate() {
 		return new ExRsaTemplate();
 	}
 
 	@Bean
 	@ConditionalOnClass(EcTemplate.class)
-	public EcTemplate ecTemplate() {
+	EcTemplate ecTemplate() {
 		return new EcTemplate();
 	}
 
 	@Bean
 	@ConditionalOnProperty(prefix = "heanbian.email", name = "enabled", havingValue = "true")
-	public EmailTemplate emailTemplate(com.heanbian.boot.autoconfigure.EmailProperties ep) {
+	EmailTemplate emailTemplate(com.heanbian.boot.autoconfigure.EmailProperties ep) {
 		return new EmailTemplate(
 				new EmailConfig(ep.host(), ep.port(), ep.username(), ep.password(), ep.from(), ep.debug()));
 	}
 
 	@Bean
 	@ConditionalOnClass(JwtTemplate.class)
-	public JwtTemplate jwtTemplate() {
+	JwtTemplate jwtTemplate() {
 		return new JwtTemplate();
 	}
-	
-	@Bean
-	@ConditionalOnProperty(prefix = "heanbian.oss", name = "enabled", havingValue = "true")
-	public OSS oss(com.heanbian.boot.autoconfigure.OssProperties op) {
-		return new OSSClientBuilder().build(op.endpoint(), op.accessKeyId(), op.accessKeySecret());
-	}
+
 }
